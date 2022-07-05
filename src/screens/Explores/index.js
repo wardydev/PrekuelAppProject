d7f99f2b327714718c035c6f638eb2f3e89e24b6
wardy30 <hairul.wardy@digitalent.id>
2022-07-06 06:40:43 +0800

    12. add isSearch logic

--- src/screens/Explores/index.js
+++ src/screens/Explores/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {TextInput, StyleSheet, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, StyleSheet, ScrollView, View, Text} from 'react-native';
 
 import WrapperScreen from '../../components/WrapperScreen';
 import HeadingScreen from '../../components/atomic/HeadingScreen';
@@ -7,18 +7,58 @@ import {primaryFont} from '../../utils/constants/fontSize';
 import TagCategories from '../../components/TagCategories';
 import HeaderTitle from '../../components/HeaderTitle';
 import ArticleList from '../../components/ArticleLists';
+import useFetch from '../../hooks/useFetch';
 
 const Explores = ({navigation}) => {
+  const {data, loading, error} = useFetch(
+    'https://prekuel.com/wp-json/wp/v2/posts?per_page=100',
+  );
+  const [dataFiltered, setDataFiltered] = useState();
+  const [isSearch, setIsSearch] = useState(false);
+  const [search, setSearch] = useState('');
+
+  const filteredData = text => {
+    setSearch(text);
+    setIsSearch(true);
+
+    if (text === '') {
+      setIsSearch(false);
+    }
+
+    if (text) {
+      const newData = data.filter(item => {
+        return (
+          item.title.rendered.toLowerCase().includes(text) ||
+          item.better_featured_image.alt_text.toLowerCase().includes(text) ||
+          item.better_featured_image.description.toLowerCase().includes(text) ||
+          item.content.rendered.toLowerCase().includes(text)
+        );
+      });
+      setDataFiltered(newData);
+    }
+  };
+
   const url =
     'https://prekuel.com/wp-json/wp/v2/posts?categories=38&per_page=5';
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <WrapperScreen>
         <HeadingScreen heading="Explores" paragraph="Explore your news" />
-        <TextInput style={styles.input} placeholder="Cari berita mu disini.." />
-        <TagCategories navigation={navigation} />
-        <HeaderTitle isSeeAll={true} title="Rekomendasi" />
-        <ArticleList url={url} navigation={navigation} isPost={false} />
+        <TextInput
+          style={styles.input}
+          placeholder="Cari berita mu disini.."
+          onChangeText={text => filteredData(text)}
+          value={search}
+        />
+        {isSearch ? (
+          <Text style={{backgroundColor: 'green'}}>Search result..</Text>
+        ) : (
+          <View>
+            <TagCategories navigation={navigation} />
+            <HeaderTitle isSeeAll={true} title="Rekomendasi" />
+            <ArticleList url={url} navigation={navigation} isPost={false} />
+          </View>
+        )}
       </WrapperScreen>
     </ScrollView>
   );
